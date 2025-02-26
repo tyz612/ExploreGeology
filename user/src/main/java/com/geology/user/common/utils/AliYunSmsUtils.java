95d465ebeb2b1aa256094d717ce4aa74fafd3cad
tyz <tyz.bnu@gmail.com>
2025-02-26 16:26:09 +0800

    分页,poi,返回体优化

--- user/src/main/java/com/geology/user/common/utils/AliYunSmsUtils.java
+++ user/src/main/java/com/geology/user/common/utils/AliYunSmsUtils.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AliYunSmsUtils {
+    public static String accessKeyId = "LTAI5tKfKGwzvtVEkYJCEkdX";
+    public static String accessSecret = "I0fP5udXR9K0oTGVJNQF3RWypXwRED";
     public static String signName = "geology0612";
     public static String TemplateCode = "SMS_476835066";
     public static final String PRODUCT = "Dysmsapi";
