21bfbac907bd3f6adc3e761350b6276497248161
tyz <tyz.bnu@gmail.com>
2025-09-28 11:07:08 +0800

    轨迹上传兼容多种结构的kml

--- data/src/main/java/com/geology/common/utils/KmlParseUtil.java
+++ data/src/main/java/com/geology/common/utils/KmlParseUtil.java
@@ -14,8 +14,12 @@ import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -33,8 +37,22 @@ public class KmlParseUtil {
         // 解析元数据
         trajectory.setName(doc.select("Document > name").text());
 
+        Element description = null;
+        Element extent = null;
         // 解析轨迹描述信息
-        Element description = doc.select("Placemark > description").first();
+        Elements descriptions = doc.select("Placemark > description");
+        for (int i = 0; i < descriptions.size(); i++){
+            String descriptionTemp = descriptions.get(i).text();
+            if (descriptionTemp.contains("开始时间"))
+            {
+                description = descriptions.get(i);
+            }
+            else
+            {
+                continue;
+            }
+        }
+        Elements extents = doc.select("ExtendedData");
         if (description != null) {
             // 获取description内所有div元素
             Elements divs = description.select("div");
@@ -114,6 +132,95 @@ public class KmlParseUtil {
                     }
                 }
             }
+        }
+        else {
+            for (int i = 0; i < extents.size(); i++)
+            {
+                String extentElement = extents.get(i).toString();
+                if (extentElement.contains("BeginTime")){
+                    extent = extents.get(i);
+                }
+                else {
+                    continue;
+                }
+            }
+            Elements datas = extent.select("data");
+            for (Element div : datas) {
+                String text = div.toString().trim();
+                // 检查是否为最高海拔行
+                if (text.contains("Mileage")) {
+                    // 提取冒号后的内容并移除单位"米"
+                    String valuePart = text.split("\n")[1].trim();  // 使用中文冒号分割
+//                    String totalDistance = valuePart.replace("米", "").trim();
+                    // 转换为double类型
+                    try {
+                        Float distance = Float.parseFloat(valuePart);
+                        trajectory.setDistance(distance);
+                    } catch (NumberFormatException e) {
+                        log.info("里程转换失败: " + valuePart);
+                    }
+                }
+            }
+
+            for (Element div : datas) {
+                String text = div.toString().trim();
+                // 检查是否为最高海拔行
+                if (text.contains("BeginTime")) {
+                    // 提取冒号后的内容并移除单位"米"
+                    String valuePart = text.split("\n")[1].trim();  // 使用中文冒号分割
+//                    String totalDistance = valuePart.replace("米", "").trim();
+                    // 转换为double类型
+                    try {
+                        Instant instant = Instant.ofEpochMilli(Long.parseLong(valuePart));
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                .withZone(ZoneId.systemDefault()); // 使用系统默认时区
+                        String formattedDate = formatter.format(instant);
+                        trajectory.setStartTime(formattedDate);
+                    } catch (NumberFormatException e) {
+                        log.info("里程转换失败: " + valuePart);
+                    }
+                }
+            }
+
+            for (Element div : datas) {
+                String text = div.toString().trim();
+                // 检查是否为最高海拔行
+                if (text.contains("EndTime")) {
+                    // 提取冒号后的内容并移除单位"米"
+                    String valuePart = text.split("\n")[1].trim();  // 使用中文冒号分割
+//                    String totalDistance = valuePart.replace("米", "").trim();
+                    // 转换为double类型
+                    try {
+                        Instant instant = Instant.ofEpochMilli(Long.parseLong(valuePart));
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                .withZone(ZoneId.systemDefault()); // 使用系统默认时区
+                        String formattedDate = formatter.format(instant);
+                        trajectory.setEndTime(formattedDate);
+                    } catch (NumberFormatException e) {
+                        log.info("里程转换失败: " + valuePart);
+                    }
+                }
+            }
+
+            // 解析轨迹点
+            Elements coords = doc.select("gx|coord");
+            List<Float> elevationList = new ArrayList<>();
+            for (Element coord : coords) {
+                String[] values = coord.text().split("\\s+");
+                if (values.length >= 3) {
+                    Float elevation = Float.parseFloat(values[2]);
+                    elevationList.add(elevation);
+                }
+            }
+            if (!elevationList.isEmpty()) {
+                Float minElevation = Collections.min(elevationList);
+                Float maxElevation = Collections.max(elevationList);
+                trajectory.setMinAltitude(minElevation);
+                trajectory.setMaxAltitude(maxElevation);
+            } else {
+                log.info("海拔为空");
+            }
+
         }
 
         // 解析轨迹点
